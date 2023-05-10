144bf6583a93f275be789517a911912a3959c300
svelallanos <svelallanos@gmail.com>
2023-05-10 17:46:04 +0000

    dd

--- Views/Portalweb/normatividad.php
+++ Views/Portalweb/normatividad.php
@@ -1,39 +1,54 @@
 <?php headerPortal($data); ?>
 <section class="container-section">
     <div class="__card__normatividad">
-        <div class="__card__title">
-            <h3>Normas Municipales</h3>
+        <div class="__card__slider">
+            <img src="<?= media() ?>/portalweb/images/bg-hero.jpg" alt="">
         </div>
-        <div class="__card__table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First</th>
-                        <th>Last</th>
-                        <th>Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <td>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="__card__title">
+            <div class="__contenido__title">
+                <h3>Normas Municipales</h3>
+            </div>
+        </div>
+        <div class="__card__grid bg-primary">
+            <div class="__card__informacion bg-warning">
+                <div class="__card__filtro">
+                    <h3>filtro</h3>
+                </div>
+                <div class="__card__table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First</th>
+                                <th>Last</th>
+                                <th>Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th>2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th>3</th>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="__card__enlaces bg-success">
+                holas
+            </div>
         </div>
     </div>
 
