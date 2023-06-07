5e59d7f6a1aef8afdaecbb2e08dfbe3f794c2b73
svelallanos <svelallanos@gmail.com>
2023-06-07 12:32:46 -0500

    ddd

--- Views/Portalweb/alcalde.php
+++ Views/Portalweb/alcalde.php
@@ -22,14 +22,32 @@
             </div>
         </div>
         <div class="__data__saludo">
-            <div class="__saludo">
-                <h1>SALUDO <span>Municipalidad Distrital de Elías Soplín Vargas</span></h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ut consequatur soluta delectus quod adipisci eveniet nisi, veritatis sequi odio laboriosam repellat accusantium dignissimos voluptatibus amet repellendus explicabo in pariatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laboriosam omnis, at neque nam nostrum ab quae officiis nulla quasi ex nihil expedita, ea ratione! Nesciunt dolore rerum sequi a!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi cum recusandae ea! Voluptas minus sequi incidunt magnam ad accusamus nihil dolorum quaerat consequatur ab obcaecati consequuntur, error odit perferendis tenetur.lorenm</p>
+            <div class="__title">
+                <div class="__opciones">
+                    <h1>SALUDO</h1>
+                    <button data-bs-toggle="modal" data-bs-target="#view__vitae__alcalde"><i class="feather-file-text"></i>&nbsp;&nbsp;Hoja de Vida</button>
+                </div>
+                <span>Municipalidad Distrital de Elías Soplín Vargas</span>
             </div>
-            <div class="__vitae">
-                <iframe src="https://www.cordivino.com/gallery/sangre%20de%20campeones.pdf" frameborder="0"></iframe>
+            <div class="__saludo">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ut consequatur soluta delectus quod adipisci eveniet nisi, veritatis sequi odio laboriosam repellat accusantium dignissimos voluptatibus amet repellendus explicabo in pariatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laboriosam omnis, at neque nam nostrum ab quae officiis nulla quasi ex nihil expedita, ea ratione! Nesciunt dolore rerum sequi a!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi cum recusandae ea! Voluptas minus sequi incidunt magnam ad accusamus nihil dolorum quaerat consequatur ab obcaecati consequuntur, error odit perferendis tenetur.lorenm</p>
             </div>
         </div>
     </div>
 </section>
+
+<!-- Modal -->
+<div class="modal fade" id="view__vitae__alcalde" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: var(--amarillo-4);">
+                <h1 class="modal-title fs-1 fw-bold" id="exampleModalLabel">Hoja de vida - <span style="color: var(--azul-4);">ALCALDE</span></h1>
+                <button type="button" class="btn-close bg-light border border-1 border-warning p-3" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-0" style="height: 50rem;">
+                <iframe style="border-radius: 0 0 0.5rem 0.5rem;" class="w-100 h-100" src="https://germarmu.files.wordpress.com/2014/09/resumen-cien-ac3b1os-de-soledad-mc3a1rquez.pdf" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
 <?php footerPortal(); ?>
