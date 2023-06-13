5b248d97bbcff95b41b159c7000ec60bf77a83f0
svelallanos <svelallanos@gmail.com>
2023-06-13 08:43:42 -0500

    ss

--- Models/GestionModel.php
+++ Models/GestionModel.php
@@ -10,7 +10,7 @@ class GestionModel extends Mysql
     // Select All
     public function selectsGestion()
     {
-        $sql = 'SELECT * FROM gestion';
+        $sql = 'SELECT * FROM gestion_municipal';
         $request = $this->select_all($sql, array(), DB_PORTAL);
 
         return $request;
@@ -20,7 +20,7 @@ class GestionModel extends Mysql
 
     public function saveGestion(int $usuarios_id, string $gestion_nombre, string $gestion_descripcion, string $gestion_fecha_inicio, string $gestion_fecha_final)
     {
-        $sql = "INSERT INTO gestion (usuarios_id, gestion_nombre, gestion_descripcion, gestion_fecha_inicio, gestion_fecha_final) VALUES (:usuarios_id, :gestion_nombre, :gestion_descripcion, :gestion_fecha_inicio, :gestion_fecha_final)";
+        $sql = "INSERT INTO gestion_municipal (usuarios_id, gestion_nombre, gestion_descripcion, gestion_fecha_inicio, gestion_fecha_final) VALUES (:usuarios_id, :gestion_nombre, :gestion_descripcion, :gestion_fecha_inicio, :gestion_fecha_final)";
 
         $arrData = [
             'usuarios_id' => $usuarios_id,
@@ -39,7 +39,7 @@ class GestionModel extends Mysql
 
     public function updateGestion(int $gestion_id, string $gestion_nombre, string $gestion_descripcion, string $gestion_fecha_inicio, string $gestion_fecha_final)
     {
-        $sql = 'UPDATE gestion SET gestion_nombre = :gestion_nombre,gestion_descripcion = :gestion_descripcion, gestion_fecha_inicio = :gestion_fecha_inicio, gestion_fecha_final = :gestion_fecha_final 
+        $sql = 'UPDATE gestion_municipal SET gestion_nombre = :gestion_nombre,gestion_descripcion = :gestion_descripcion, gestion_fecha_inicio = :gestion_fecha_inicio, gestion_fecha_final = :gestion_fecha_final 
         WHERE gestion_id = :gestion_id';
 
         $arrData = [
@@ -60,7 +60,7 @@ class GestionModel extends Mysql
 
     public function deleteGestion(int $gestion_id)
     {
-        $sql = 'DELETE FROM gestion
+        $sql = 'DELETE FROM gestion_municipal
         WHERE gestion_id =:gestion_id';
 
         $request = $this->delete($sql, ['gestion_id' => $gestion_id], DB_PORTAL);
