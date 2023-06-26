f9f5f0a1084c08abc2b97f7fdb5ed89b2d75ed3c
svelallanos <svelallanos@gmail.com>
2023-06-26 17:55:57 -0500

    ss

---
+++ principal.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./principal.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Mucipalidad Distrital de Elías Soplín Vargas</title>
+</head>
+
+<body>
+    <header class="container-fluid">
+        <div class="header_bg">
+            <div class="header_social container-xl">
+                <div class="logos">
+                    <div class="logo">
+                        <img src="./Assets/images/Logo-MDESV.png" alt="">
+                    </div>
+                    <div class="alcalde">
+                        <img src="./Assets/images/Logo-MDESV.png" alt="">
+                    </div>
+                </div>
+                <div class="social">
+                    hola
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <main></main>
+    <footer></footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
