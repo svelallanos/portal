70cc6cf1edd454bb6b04437928b1eff5c92894bb
svelallanos <svelallanos@gmail.com>
2023-06-27 10:08:52 -0500

    sss

--- principal.php
+++ principal.php
@@ -13,24 +13,40 @@
     <header class="container-fluid">
         <div class="header_bg">
             <div class="header_social container-xl">
-                <div class="logos">
-                    <div class="logo">
-                        <img src="./Assets/images/Logo-MDESV.png" alt="">
-                    </div>
-                    <div class="alcalde">
-                        <img src="./Assets/images/Logo-MDESV.png" alt="">
-                    </div>
+                <div class="logo">
+                    <img src="./Assets/images/Insignia-MDESV.png" alt="">
+                </div>
+                <div class="detalle">
+                    <h1>Municipalidad Distrital De Elías Soplín Vargas</h1>
+                    <h3>Segunda Jerusalén</h3>
                 </div>
                 <div class="social">
-                    hola
+                    <div class="email">
+                        <a href="mailto:"><i class="fa-regular fa-envelope"></i>&nbsp;mesadepartes@munieliassoplinvargas.gob.pe</a>
+                    </div>
+                    <div class="telefono">
+                        <a href="tel:+"><i class="fa-solid fa-phone"></i>&nbsp;+51 999 999 999</a>
+                    </div>
+                    <div class="redes">
+                        <a href="https://www.facebook.com/md.segundajerusalen/?locale=es_LA" target="_blank"><i class="fa-brands fa-facebook-f fa-beat"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram fa-beat"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube fa-beat"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
-
     </header>
-    <main></main>
-    <footer></footer>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.js"></script>
+    <main class="container-fluid">
+        <div class="main_bg container-xl">
+            <img class="w-100" src="./Assets/portalweb/images/portal_principal.jpg" alt="">
+        </div>
+    </main>
+    <footer class="container-fluid">
+        <div class="footer_bg container-xl">
+            <a href="https://www.munieliassoplinvargas.gob.pe/portal/">PAGINA WEB INSTITUCIONAL</a>
+        </div>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
