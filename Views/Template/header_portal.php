f534405a19c33c49a2fe0449367843fe9510d001
svelallanos <svelallanos@gmail.com>
2023-06-27 10:50:31 -0500

    sss

--- Views/Template/header_portal.php
+++ Views/Template/header_portal.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal Institucional</title>
+    <title>:. Portal Institucional</title>
+    <link rel="icon" type="image/x-icon" href="<?= media() ?>/images/Insignia-MDESV.png?version=<?= getVersion() ?>" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
     <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
