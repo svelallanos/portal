5f9b1409fc71e7f48329d49cffe1d40c68d900b4
svelallanos <svelallanos@gmail.com>
2023-06-27 10:40:46 -0500

    sss

--- principal.php
+++ principal.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./principal.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Mucipalidad Distrital de Elías Soplín Vargas</title>
+    <link rel="icon" type="image/x-icon" href="./Assets/images/Insignia-MDESV.png" />
 </head>
 
 <body>
