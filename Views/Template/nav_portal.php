1bd7077babc99a4b1dd88127fe5b8daf55e22c52
svelallanos <svelallanos@gmail.com>
2023-06-30 17:30:31 -0500

    SSSS

--- Views/Template/nav_portal.php
+++ Views/Template/nav_portal.php
@@ -42,12 +42,16 @@
 <header class="cabecera-navb">
     <div class="container-navb">
         <div class="navb-logo">
-            <img src="<?= media() ?>/portalweb/images/logo.webp" alt="">
+            <img src="<?= media() ?>/portalweb/images/muni_logob.png" alt="">
         </div>
         <nav class="navbar">
             <ul>
                 <li><a class="active" href="<?= base_url() ?>">Inicio</a></li>
-                <li><a href="<?= base_url() ?>">Nosotros</a></li>
+                <li><a href="<?= base_url() ?>">Segunda Jerusalén <i class="fa-solid fa-angle-up __drop"></i></a>
+                    <ul>
+                        <li><a href="<?= base_url() ?>">Tioyacu</a></li>
+                    </ul>
+                </li>
                 <li>
                     <a href="#">Municipalidad <i class="fa-solid fa-angle-up __drop"></i></a>
                     <ul>
@@ -80,10 +84,10 @@
                         <li><a href="<?= base_url() ?>portalweb/convenios">Convenios</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Distrito <i class="fa-solid fa-angle-up __drop"></i></a>
-                    <ul>
+                <li><a href="#">Transparencia <i class="fa-solid fa-angle-up __drop"></i></a>
+                    <!-- <ul>
                         <li><a href="<?= base_url() ?>">Tioyacu</a></li>
-                    </ul>
+                    </ul> -->
                 </li>
             </ul>
 
@@ -91,7 +95,47 @@
                 <div class="item-icon"><i class="feather-search"></i></div>
                 <div class="item-icon"><a href="<?= base_url() ?>login"><i class="feather-log-in"></i></a></div>
                 <div class="item-icon"><img src="<?= media() ?>/portalweb/images/transparencia_icon.png" alt=""></div>
+                <div class="item-icon"><button type="button" class="button_sidebar" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"><i class="feather-align-justify"></i></button></div>
             </div>
         </nav>
     </div>
+</header>
+<header class="sidebar_portal">
+    <nav class="navbar bg-light fixed-top p-0">
+        <div class="container-fluid">
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <img src="<?= media() ?>/portalweb/images/muni_logob.png" alt="">
+                    <button class="button_close" data-bs-dismiss="offcanvas"><i class="feather-x"></i></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
